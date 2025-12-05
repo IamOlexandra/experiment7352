6587e9e2ce947625d3a16283f2d8f1ef983f934d
IamOlexandra <alex.eve.kravchenko@gmail.com>
2025-12-05 18:11:19 +0200

    Change a lot of stuff; rename ssp to rps

--- src/js/olexandra.js
+++ src/js/olexandra.js
@@ -22,3 +22,24 @@ document.getElementById("header-opening").addEventListener("click", () => {
     isHeaderSublistVisible ? headerSublist.style.display = "none" : headerSublist.style.display = "block";
     isHeaderSublistVisible = !isHeaderSublistVisible;
 });
+
+const yearResult = document.querySelector(".year-text");
+document.querySelector(".year-button").addEventListener("click", () => {
+    const year = Number(document.querySelector(".year-input").value.trim());
+    if(isNaN(year)) {
+        yearResult.textContent = "Введіть справжнє число число";
+        yearResult.style.color = "var(--main)";
+    }
+    else if((year % 4) === 0) {
+        yearResult.textContent = "Ви народилися у високосний рік!";
+        yearResult.style.color = "var(--win)";
+    } else {
+        yearResult.textContent = "Ви народилися не у високосний рік!";
+        yearResult.style.color = "var(--loss)";
+    }
+});
+
+const rpsResult = document.querySelectorAll(".rps-text"),
+    rpsButtons = querySelectorAll(".rps-itembutton");
+let pressedRpsButton = null;
+fu
