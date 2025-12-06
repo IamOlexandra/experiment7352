e5f849cf0f4b5ff27fa22ed6ba0c125116b160f7
IamOlexandra <alex.eve.kravchenko@gmail.com>
2025-12-06 20:03:55 +0200

    Almost finish calculator

--- src/js/olexandra.js
+++ src/js/olexandra.js
@@ -39,7 +39,53 @@ document.querySelector(".year-button").addEventListener("click", () => {
     }
 });
 
-const rpsResult = document.querySelectorAll(".rps-text"),
-    rpsButtons = querySelectorAll(".rps-itembutton");
-let pressedRpsButton = null;
-fu
+const rpsResult = document.querySelector(".rps-text"),
+    rpsButtons = document.querySelectorAll(".rps-itembutton");
+let pressedRpsButton = null,
+    rpsComputerChose = null,
+    rpsComputerWins = 0,
+    rpsPlayerWins = 0;
+rpsButtons.forEach((button, index) => button.addEventListener("click", () => {
+    if(rpsComputerChose !== null) {
+        rpsButtons[rpsComputerChose].classList.remove("rps-itembutton-computer");
+    }
+    if(pressedRpsButton !== null) {
+        rpsButtons[pressedRpsButton].classList.remove("rps-itembutton-pressed");
+    }
+    pressedRpsButton = index;
+    rpsButtons[pressedRpsButton].classList.add("rps-itembutton-pressed");
+    rpsComputerChose = Math.floor(Math.random() * 3);
+    if(rpsComputerChose - index === -1 || rpsComputerChose - index === 2) {
+        rpsResult.textContent = "Комп’ютер виграв раунд!";
+        rpsResult.style.color = "var(--loss)";
+        rpsComputerWins++;
+    } else if(rpsComputerChose - index === 1 || rpsComputerChose - index === -2) {
+        rpsResult.textContent = "Ви виграли раунд!";
+        rpsResult.style.color = "var(--win)";
+        rpsPlayerWins++;
+    } else if(rpsComputerChose === index) {
+        rpsResult.textContent = "Нічія!";
+        rpsResult.style.color = "var(--draw)";
+    }
+    document.querySelector(".rps-score").innerHTML = `Рахунок:<br>Комп’ютер - ${rpsComputerWins}<br>Ви - ${rpsPlayerWins}`;
+}));
+document.querySelector(".rps-button").addEventListener("click", () => {
+    if(rpsComputerChose !== null) {
+        rpsButtons[rpsComputerChose].classList.add("rps-itembutton-computer");
+    }
+});
+
+const calculatorResult = document.querySelector(".calculator-text"),
+    calculatorButtons = document.querySelectorAll(".calculator-actionbutton");
+let pressedCalculatorButton = null;
+calculatorButtons.forEach((button, index) => button.addEventListener("click", () => {
+    if(pressedCalculatorButton !== null) {
+        calculatorButtons[pressedCalculatorButton].classList.remove("calculator-button-pressed");
+    }
+    pressedCalculatorButton = index;
+    calculatorButtons[pressedCalculatorButton].classList.add("calculator-button-pressed");
+}));
+document.querySelectorAll(".calculator-equals").addEventListener("click", () => {
+    const number1 = Number(document.getElementById("calculator-number1").value.trim()),
+        number2 = Number(document.getElementById("calculator-number2").value.trim());
+});
